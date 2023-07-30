aa5100f8da648634a727ac0b0203eb61fdaa048d
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-07-30 18:37:15 +0200

    deleted unnecessary get path

--- src/Pages/GetPaste.js
+++ src/Pages/GetPaste.js
@@ -31,15 +31,18 @@ const GetPaste = () => {
 
     return (
         <div className="main">
-            <CopyToClipboard text={ pasteContent }>
-                <button
-                    className="copy-button"
-                    onCopy={() => setCopied(true)}>
-                    <svg className="plus" width="1.8rem" height="1.8rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4 12H20M12 4V20" stroke="#ffffff" strokeWidth="2.3" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
-                </button>
-            </CopyToClipboard>
+            <div className="post-nav">
+                <CopyToClipboard text={ pasteContent }>
+                    <button
+                        className="copy-button"
+                        onCopy={() => setCopied(true)}>
+                        <svg className="plus" width="1.8rem" height="1.8rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4 12H20M12 4V20" stroke="#ffffff" strokeWidth="2.3" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                    </button>
+                </CopyToClipboard>
+            </div>
+            
             {pasteContent ? (
                 
                 <div class="content">
