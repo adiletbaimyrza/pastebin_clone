a805e08f925521102912fd9e7961aae5f60519e3
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-08-01 12:23:46 +0200

    code refactor

--- src/Pages/PostPaste.js
+++ src/Pages/PostPaste.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PlusSVG from '../SVGs/PlusSVG';
 
 const PostPaste = () => {
     const [content, setContent] = useState('');
@@ -67,9 +68,7 @@ const PostPaste = () => {
                 <button
                     className="paste"
                     onClick={handlePostClick}>
-                    <svg className="plus" width="1.8rem" height="1.8rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4 12H20M12 4V20" stroke="#ffffff" strokeWidth="2.3" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
+                    <PlusSVG />
                     <div className="paste-button-text">New Paste</div> 
                 </button>
             </div>
---
+++ src/SVGs/PlusSVG.js
@@ -0,0 +1,9 @@
+const PlusSVG = () => {
+    return (
+        <svg className="plus" width="1.8rem" height="1.8rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4 12H20M12 4V20" stroke="#ffffff" strokeWidth="2.3" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+};
+
+export default PlusSVG;
