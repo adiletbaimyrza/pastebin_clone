338379feecdcbbb9d486901dda07ad41ef769e86
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-08-11 15:01:25 +0200

    adding a comments section

--- src/Pages/GetPaste.js
+++ src/Pages/GetPaste.js
@@ -1,47 +1,75 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { format } from "date-fns"
 
 import CopySVG from "../SVGs/CopySVG";
 import TimeSVG from "../SVGs/TimeSVG";
 import TimerSVG from "../SVGs/TimerSVG";
 import UserSVG from "../SVGs/UserSVG";
 
-import { format } from "date-fns"
-
 const GetPaste = () => {
   const [pasteContent, setPasteContent] = useState("");
   const [pasteCreatedAt, setPasteCreatedAt] = useState("");
   const [pasteExpireAt, setPasteExpireAt] = useState("");
-  const [pasteUserId, setPasteUserId] = useState("");
   const [pasteUsername, setPasteUsername] = useState("");
-  const [copied, setCopied] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const commentData = {
+        text : text,
+        author : author
+    };
+
+    fetch('/add_comment', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`,
+        },
+        body: JSON.stringify(commentData),
+    })
+    .then((response) => {
+        if (response.ok) {
+            console.log("OK. Comment sent to the API");
+        } else {
+            console.error('Failed to create paste:', response.statusText);
+        }
+    })
+    .catch((error) => {
+        console.error('Error sending the request:', error);
+    });
+  };
 
   useEffect(() => {
-    // Extract the HASH from the address
     const pathParts = window.location.pathname.split("/");
     const hash = pathParts[pathParts.length - 1];
 
-            // Make the GET request to retrieve the paste content
-        axios
-          .get(`/${hash}`)
-          .then((response) => {
-            if (response.status === 200) {
-              setPasteContent(response.data.content);
-              // Format the datetime strings using date-fns
-              setPasteCreatedAt(format(new Date(response.data.created_at), "MMMM d, yyyy HH:mm"));
-              setPasteExpireAt(format(new Date(response.data.expire_at), "MMMM d, yyyy HH:mm"));
-              setPasteUserId(response.data.user_id);
-              setPasteUsername(response.data.username);
-            } else {
-              // Handle error cases, e.g., display an error message
-              console.error("Failed to get paste:", response.statusText);
-            }
-          })
-          .catch((error) => {
-            console.error("Error sending the request:", error);
-          });
-  }, []); // Empty dependency array ensures the effect runs only once on mount
+    axios
+      .get(`/${hash}`)
+      .then((response) => {
+        if (response.status === 200) {
+          setPasteContent(response.data.content);
+          setPasteCreatedAt(format(new Date(response.data.created_at), "MMMM d, yyyy HH:mm"));
+          setPasteExpireAt(format(new Date(response.data.expire_at), "MMMM d, yyyy HH:mm"));
+          setPasteUserId(response.data.user_id);
+          setPasteUsername(response.data.username);
+          setComments(response.data.comments);
+        } else {
+          console.error("Failed to get paste:", response.statusText);
+        }
+      })
+      .catch((error) => {
+        console.error("Error sending the request:", error);
+      });
+    }, 
+    
+  []);
 
   return (
     <div className="main">
@@ -50,14 +78,17 @@ const GetPaste = () => {
           <TimeSVG />
           {pasteCreatedAt}
         </div>
+
         <div className="post-nav-item">
           <TimerSVG />
           {pasteExpireAt}
         </div>
+
         <div className="post-nav-item">
           <UserSVG />
           {pasteUsername}
-          </div>
+        </div>
+
         <CopyToClipboard text={pasteContent}>
           <button className="copy-button" onCopy={() => setCopied(true)}>
             <CopySVG />
@@ -66,9 +97,45 @@ const GetPaste = () => {
       </div>
 
       {pasteContent ? (
-        <div className="content">
-          <pre>{pasteContent}</pre>
-        </div>
+        <>
+          <div className="content">
+            <pre>{pasteContent}</pre>
+          </div>
+          
+          <div className="comments">
+            {comments.length > 0 ? (
+              comments.map((comment, index) => (
+                <div key={index} className="comment">
+                  <p>{comment.text}</p>
+                  <p>By: {comment.author}</p>
+                </div>
+              ))
+            ) : (
+              <p>No comments available.</p>
+            )}
+          </div>
+
+          <div className="add-comment">
+            <form onSubmit={handleSubmit}>
+              <label className="add-comment-label">
+                  Comment
+                  <input
+                      className="input comment-input"
+                      value={comment}
+                      onChange={handleCommentChange}
+                      type="text"
+                      id="comment"
+                      name="comment"
+                      placeholder="comment"
+                      required
+                  />
+              </label>
+              <button className="add-comment-button button" type="submit">
+                    Add comment
+              </button>
+            </form>
+          </div>
+        </>
       ) : (
         <div>
           <p>Paste loading...</p>
