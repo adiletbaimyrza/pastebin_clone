63528bdb3084ab6c57a3eb0f63a7dff06915d25c
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-08-14 14:21:51 +0200

    deleted unneccessary hash table

--- flask_api/models.py
+++ flask_api/models.py
@@ -6,31 +6,24 @@ fs_mixin = FlaskSerialize(db)
 
 class User(db.Model, fs_mixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)    # email max 100 chars
-    username = db.Column(db.String(50), unique=True, nullable=False)  # username max 50 chars
-    password = db.Column(db.String(87), nullable=False)               # encrypted password max 87 chars
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(87), nullable=False)
     comments = db.relationship('Comment', backref='user')
     pastes = db.relationship('Paste', backref='user')
 
-
 class Paste(db.Model, fs_mixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    url_hash = db.Column(db.String(8), unique=True, nullable=False)          # url_hash max 8 chars
-    blob_url = db.Column(db.String(256), unique=True, nullable=True)        # blob_url max 256 chars
+    url_hash = db.Column(db.String(8), unique=True, nullable=False)
+    blob_url = db.Column(db.String(256), unique=True, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False)
     expire_at = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     comments = db.relationship('Comment', backref='paste')
-
-
-class Hash(db.Model, fs_mixin):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    url_hash = db.Column(db.String(8), unique=True, nullable=False)
-
-
+    
 class Comment(db.Model, fs_mixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    content = db.Column(db.String(200), nullable=False)                # comment.content max 200 chars 
+    content = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
     expire_at = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
